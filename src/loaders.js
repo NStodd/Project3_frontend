176392db7b4d692917232c4678809ba0d4fb248b
Nate Stodd <nathan.stoddard@gmail.com>
2023-01-21 15:43:51 -0500

    recipes and recipe loaders

---
+++ src/loaders.js
@@ -0,0 +1,13 @@
+const URL = "https://project3-backend-tay4.onrender.com/"
+
+export const recipesLoader = async () => {
+    const response = await fetch(URL + "/recipes")
+    const recipes = await response.json()
+    return recipes;
+}
+
+export const recipeLoader = async ({params}) => {
+    const response = await fetch (URL + "/recipes/" + params.id)
+    const recipe = await response.json()
+    return recipe;
+}
