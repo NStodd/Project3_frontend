d86768bc4b3b443e7a235ed373320d726f46ddc1
Josh Gulledge <jgulledge19@gmail.com>
2023-01-26 18:37:53 -0700

    Changed input to textarea for styling and added some styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ function Header(props) {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/">React Cookbook</Navbar.Brand>
+        <Navbar.Brand to="/">React Cookbook</Navbar.Brand>
         <Nav className="navbar">
-          <Nav.Link href='/new'>Create Recipe</Nav.Link>
+          <Nav.Link to='/new'>Create Recipe</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
